347bda1c3f290fd1ed486c0408dd1aeada8aa100
Kymberli Duffy <kymberlicodes@gmail.com>
2019-06-07 23:32:42 -0500

    More tests

--- helloWorld.py
+++ helloWorld.py
@@ -1,2 +1,18 @@
-printf("Hello world!")
+## First Python Program
+print("Hello world!")
 
+def feedTheDogs(breakfast, dinner):
+	if breakfast or dinner:
+		return True
+	else:
+		return False
+
+test1 = feedTheDogs(True, True)
+test2 = feedTheDogs(True, False)
+test3 = feedTheDogs(False, True)
+test4 = feedTheDogs(False, False)
+
+print(f'Is it time to feed the dogs? {test1}')
+print(f'Is it time to feed the dogs? {test2}')
+print(f'Is it time to feed the dogs? {test3}')
+print(f'Is it time to feed the dogs? {test4}')
